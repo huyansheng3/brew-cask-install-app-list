f77fd9dde84d621d51564315508ecf7a5cab9d7f
hys <1211904437@qq.com>
2016-11-26 17:47:56 +0800

    init

---
+++ install-hys-soft.sh
@@ -0,0 +1,21 @@
+# @Author: hys
+# @Date:   2016-11-26 17:03:35
+# @Last Modified by:   hys
+# @Last Modified time: 2016-11-26 17:43:03
+
+#!/usr/lib/env bash
+
+brew cask install foxmail  #邮箱
+brew cask install electronic-wechat  #微信
+brew cask install qq  #qq
+brew cask install thunder  #迅雷	
+brew cask install alfred   #搜索和简化工作流神器
+brew cask install electrum-dash  #全能文档手册
+brew cask install wiznote  #为知笔记
+brew cask install wechatwebdevtools  #微信小程序
+brew cask install firefox  #火狐浏览器
+brew cask install rescuetime  #统计在电脑上各类行为的时间
+brew cask install flux  #根据时间经纬度调节色温保护眼睛的神器
+brew cask install 1password  #密码管理
+brew cask install atom  #一款编辑器
+brew cask install sublime-text  #一款编辑器
